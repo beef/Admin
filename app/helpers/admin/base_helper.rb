2e6700af52db2c4f48db3ce8f77677ef8db70c8f
Steve England <steve@wearebeef.co.uk>
2009-06-22 13:19:15 +0100

    Include sortable helper

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -1,4 +1,5 @@
 module Admin::BaseHelper
+  include SortableTable::App::Helpers::ApplicationHelper
   
   def admin_page_title
     @page_title ||= controller.controller_name.titleize + (controller.action_name == 'index' ? ' ' : " | #{controller.action_name.titleize}" )   
