95f11f0f0d06d5e18ee2455a50249f48f0c9c1be
Daniel Craig <daniel@wearebeef.co.uk>
2010-01-20 12:53:14 +0000

    add html source editor to tiny mce

--- generators/admin_area_files/templates/public/javascripts/tiny-mce-config.js
+++ generators/admin_area_files/templates/public/javascripts/tiny-mce-config.js
@@ -9,7 +9,7 @@ document.observe("dom:loaded", function(){
     //"safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras",
     // Theme options
     theme_advanced_blockformats : "h2,h3,h4",
-    theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,formatselect,|,bullist,numlist,|,link,unlink,|,indent,outdent,|,justifyleft,justifycenter,justifyright,justifyfull",
+    theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,formatselect,|,bullist,numlist,|,link,unlink,|,indent,outdent,|,justifyleft,justifycenter,justifyright,justifyfull,|,code",
     theme_advanced_buttons2 : "",
     theme_advanced_buttons3 : "",
     theme_advanced_buttons4 : "",
