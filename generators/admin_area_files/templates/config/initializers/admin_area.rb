1b8eb475d2e9db385bffe8c12e7e609085e2ff6c
Daniel Craig <daniel@wearebeef.co.uk>
2010-03-22 20:41:29 +0000

    added in assets thumbnail setting

--- generators/admin_area_files/templates/config/initializers/admin_area.rb
+++ generators/admin_area_files/templates/config/initializers/admin_area.rb
@@ -1,2 +1,5 @@
 # A route should match 'admin_[entry_name]_path
 Beef::AdminArea::ADMIN_MENU = [:articles, :categories]
+
+# http://www.imagemagick.org/RMagick/doc/struct.html#Geometry
+#Asset.attachment_options[:thumbnails] =  {:large => '700x395!', :medium => '340x192!', :thumb => '80x80!', :square => '60x60!' }
