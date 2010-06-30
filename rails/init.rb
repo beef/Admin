ccd4462cd6b0cc85f5a18ddf1a2ef9ce8f0477db
Daniel Craig <daniel@wearebeef.co.uk>
2010-06-30 16:09:22 +0100

    remove clearence init code as not using clearance directly now

--- rails/init.rb
+++ rails/init.rb
@@ -4,7 +4,5 @@ require 'sortable_table'
 require 'admin_area'
 
 config.after_initialize do
-  # Put Clearance views at bottom of view paths
-  path = ActionController::Base.view_paths.find { |p| p.to_s =~ /clearance/ }
-  ActionController::Base.view_paths << ActionController::Base.view_paths.delete(path)
+
 end 
